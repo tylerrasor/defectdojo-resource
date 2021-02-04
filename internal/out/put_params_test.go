3749cfbf729d1385949956e3272f0eace376ba17
tylerrasor <trasor@gmail.com>
2021-02-04 08:34:50 -0500

    remove report type checking since it seems to 'just work' for all parsers

--- internal/out/put_params_test.go
+++ internal/out/put_params_test.go
@@ -1,7 +1,6 @@
 package out_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -24,30 +23,6 @@ func TestPutParamsValidateNoReportType(t *testing.T) {
 	err := params.ValidateParams()
 	assert.NotNil(t, err)
 	assert.EqualError(t, err, "Required parameter `report_type` not supplied.")
-}
-
-func TestPutParamsValidateInvalidType(t *testing.T) {
-	report_type := "invalid"
-	params := out.PutParams{
-		ReportType: report_type,
-	}
-
-	err := params.ValidateParams()
-	assert.NotNil(t, err)
-	expected := fmt.Sprintf("The specified report type, `%s`, is not a supported by Defectdojo (check that your format matches expected)", report_type)
-	assert.EqualError(t, err, expected)
-}
-
-func TestPutParamsValidateNotYetImplemented(t *testing.T) {
-	report_type := "Burp Scan"
-	params := out.PutParams{
-		ReportType: report_type,
-	}
-
-	err := params.ValidateParams()
-	assert.NotNil(t, err)
-	expected := fmt.Sprintf("The specified report type, `%s`, hasn't been implemented yet (pull requests welcome!)", report_type)
-	assert.EqualError(t, err, expected)
 }
 
 func TestPutParamsValidateNoReportPath(t *testing.T) {
